3e7845b56909ee1e8e8bb42be822d5f1fb0fabce
Kieran Dunch <dunch.kieran@gmail.com>
2022-12-19 09:20:32 -0500

    search  bar done

---
+++ jammming/src/Components/App/App.js
@@ -0,0 +1,19 @@
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>);
+}
+
+export default App;
